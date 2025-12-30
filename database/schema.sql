d1f690a34d9d642492a671f5bc983e27a57993dd
namhooh <90330798+namhooh@users.noreply.github.com>
2025-12-30 11:12:27 +0000

    Refactor schema.sql to define new tables and types
    Updated table definitions to use VARCHAR instead of TEXT for certain fields and added new tables for roles, contractors, and contract-related entities.

--- database/schema.sql
+++ database/schema.sql
@@ -4,16 +4,90 @@
 
 CREATE TABLE organization (
   id BIGSERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
+  name VARCHAR NOT NULL,
+  address VARCHAR NOT NULL,
+  country VARCHAR NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE role (
+  id BIGSERIAL PRIMARY KEY,
+  organization_id BIGSERIAL,
+  name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE contractor (
+  id BIGSERIAL PRIMARY KEY,
+  project_id BIGSERIAL,
+  contractor_type_id BIGSERIAL,
+  name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
+  address VARCHAR NOT NULL,
+  country VARCHAR NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE contractor_type (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  code VARCHAR NOT NULL,
+  description VARCHAR,
   created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 CREATE TABLE project (
   id BIGSERIAL PRIMARY KEY,
-  organization_id BIGINT,
-  name TEXT NOT NULL,
+  organization_id BIGSERIAL,
+  name VARCHAR NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
+
+CREATE TABLE contract (
+  id BIGSERIAL PRIMARY KEY,
+  organization_id BIGSERIAL,
+  project_id BIGSERIAL,
+  name VARCHAR NOT NULL,
+  description VARCHAR,
+  contract_counterparty_id BIGSERIAL,
+  contract_type_id BIGSERIAL,
+  contract_status_id BIGSERIAL,
+  effective_date DATE,
+  end_date DATE,
+  file_location URL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+  updated_by_id BIGSERIAL, 
+  version INTEGER
+);
+
+CREATE TABLE contract_counterparty (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  address VARCHAR NOT NULL,
+  country VARCHAR NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE contract_type (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  code VARCHAR NOT NULL,
+  description VARCHAR,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE contract_status (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  code VARCHAR NOT NULL,
+  description VARCHAR,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+
+
 
 -- =========================
 -- SECTION 2: RELATIONSHIPS
